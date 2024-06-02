9e6cafeab4e16331ae391b6a176728b191d2e745
clarkera <rclarke9941@gmail.com>
2024-06-02 16:57:35 -0400

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,9 +4,9 @@ INSERT INTO department (name) VALUES -- Insert values into the 'department' tabl
 ('Finance');
 
 INSERT INTO role (title, salary, department_id) VALUES -- Insert values into the 'role' table
-('Salesperson', 50000, 1),
-('Engineer', 70000, 2),
-('Accountant', 60000, 3);
+('Salesperson', 55000, 1),
+('Engineer', 80000, 2),
+('Accountant', 90000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES -- Insert values into the 'employee' table
 ('Amy', 'Davis', 1, NULL),
